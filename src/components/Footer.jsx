717d5332e54ff8089502466dab3b0c164fcecefb
jess-amelie <jess-amelie@mindmatcher.org>
2024-09-11 18:32:28 +0200

    set up and style the footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,4 +1,9 @@
+import styles from "./Footer.module.scss";
+
 export const Footer = () => {
 
-    return <div className="b3"> Footer </div>
+    return (
+    <footer className={`${styles.footer} d-flex fex-row align-items-center justify-content-center p-20`}>
+        <p>Copyright © My Cook Chef JessAm, Inc</p>
+         </footer>)
 }
