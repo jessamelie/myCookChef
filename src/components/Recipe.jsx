0f5c1e7e6a48294e70a3bfcff733d6155b382c1e
jess-amelie <jess-amelie@mindmatcher.org>
2024-10-16 21:39:54 +0200

    add Like feature

--- src/components/Recipe.jsx
+++ src/components/Recipe.jsx
@@ -1,14 +1,24 @@
+import { useState } from "react";
 import styles from "./Recipe.module.scss";
 
-export const Recipe = ({title, image}) => {
+export const Recipe = ({ title, image }) => {
+  const [liked, setLiked] = useState(false);
+
+  const handleLikedClick = () => {
+    setLiked(!liked);
+  };
+
   return (
-    <div className={styles.recipe}>
+    <div onClick={handleLikedClick} className={styles.recipe}>
       <div className={styles.imageContainer}>
         <img src={image} alt="pancake" />
       </div>
-      <div className={`${styles.recipeTitle} d-flex flex-column justify-content-center align-items-center`}>
+      <div
+        className={`${styles.recipeTitle} d-flex flex-column justify-content-center align-items-center`}
+      >
         <h3 className="mb-10">{title}</h3>
-        <i className="fa-solid fa-heart"></i>
+        <i className={`fa-solid fa-heart ${liked ? "text-primary" : ""}`}></i>
+
       </div>
     </div>
   );
