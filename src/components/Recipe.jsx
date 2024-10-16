befcc3cc039c73bb23ae367f0ace0443c0c5e11f
jess-amelie <jess-amelie@mindmatcher.org>
2024-10-16 21:18:47 +0200

    add Like icons

--- src/components/Recipe.jsx
+++ src/components/Recipe.jsx
@@ -6,8 +6,9 @@ export const Recipe = ({title, image}) => {
       <div className={styles.imageContainer}>
         <img src={image} alt="pancake" />
       </div>
-      <div className={`${styles.recipeTitle} d-flex flex-row justify-content-center align-items-center`}>
-        <h3>{title}</h3>
+      <div className={`${styles.recipeTitle} d-flex flex-column justify-content-center align-items-center`}>
+        <h3 className="mb-10">{title}</h3>
+        <i className="fa-solid fa-heart"></i>
       </div>
     </div>
   );
