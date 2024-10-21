21a12394b00175f8de8a50c7d6c8ad36f8f1dcef
jess-amelie <jess-amelie@mindmatcher.org>
2024-10-21 17:58:20 +0200

    add and stylize Search bar

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -8,7 +8,11 @@ export const Content = () => {
   return (
     <div className="flex-fill container p-20">
       <h1 className="my-30">Découvrez nos nouvelles recettes</h1>
-      <div className={`card p-20 ${styles.contentCard}`}>
+      <div className={`card d-flex flex-column p-20 ${styles.contentCard}`}>
+        <div className={`d-flex flex-row justify-content-center align-item-center my-30 ${styles.searchBar}`}>
+        <i className="fa-solid fa-magnifying-glass mr-15"></i>
+          <input className="flex-fill" type="text" placeholder="Rechercher" />
+        </div>
         <div className={styles.grid}>
           {recipes.map((r) => (
             <Recipe key={r._id} title={r.title} image={r.image} />
